521eb3c2dc27f76bcd1b8efdac0f70a76bf50c49
perezpulsan <perezpulsan@gmail.com>
2019-03-11 11:16:06 +0100

    changed cb to promise

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 const fs = require("fs");
 const httpRequest = require("request");
+const util = require("util");
+const requestPromise = util.promisify(httpRequest);
 
-httpRequest(`https://spielhoe.shaula.uberspace.de/drive/out4.json`, (error, httpResponse, body) => {
-  body = JSON.parse(body);
+requestPromise(`https://spielhoe.shaula.uberspace.de/drive/out4.json`)
+  .then(response => {
+    body = JSON.parse(response.body);
 
-  let string = Object.keys(body[0]).join(",") + "\n";
-  body.forEach(post => {
-    string +=
-      Object.values(post)
-        .join(",")
-        .replace(/\n/g, " ") + "\n";
+    let string = Object.keys(body[0]).join(",") + "\n";
+    body.forEach(post => {
+      string +=
+        Object.values(post)
+          .join(",")
+          .replace(/\n/g, " ") + "\n";
+    });
+    fs.writeFile("./dynamic.csv", string, () => console.log(`Im done with ${body.length} lines`));
+    //console.log(body);
+
+    //body = JSON.parse(body);
+    body = body.filter(d => d.noxevent > 100);
+    console.log(body);
+
+    string = Object.keys(body[0]).join(",") + "\n";
+    body.forEach(post => {
+      string +=
+        Object.values(post)
+          .join(",")
+          .replace(/\n/g, " ") + "\n";
+    });
+    fs.writeFile("./dynamicFiltered.csv", string, () => console.log(`Im done with ${body.length} lines`));
+  })
+  .catch(err => {
+    console.log(err);
   });
-  fs.writeFile("./dynamic.csv", string, () => console.log(`Im done with ${body.length} lines`));
-  //console.log(body);
-
-  //body = JSON.parse(body);
-  body = body.filter(d => d.noxevent > 100);
-  console.log(body);
-
-  string = Object.keys(body[0]).join(",") + "\n";
-  body.forEach(post => {
-    string +=
-      Object.values(post)
-        .join(",")
-        .replace(/\n/g, " ") + "\n";
-  });
-  fs.writeFile("./dynamicFiltered.csv", string, () => console.log(`Im done with ${body.length} lines`));
-});
